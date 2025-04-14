c324a8f2c2f7dbb2280508f7ca8cf327f6f6f465
arielmusa <arielmusainfo@gmail.com>
2025-04-14 15:35:32 +0200

    add missing .length in if statement for randomNumbers.length > correctValues.length

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// inizio con la selezione degli elementi del DOM
-// visualizzare in pagina 5 numeri casuali:
-// necessito di una funzione math.random che utilizzo per generare i numeri ed inserirli dentro un array
-// l'array mi serve dopo per verificare se l'input dell'utente è corretto
-// il math.random dovrà generare un numero. poi mi serve anche una funzione per generare n numeri. n è un parametro della funzione
-// dopo faccio un ciclo ed inserisco ogni numero casuale come <li> dentro la <ul>
-// ------
-// Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input
-// in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce:
-// utilizzo il timeout per il timer e per far comparire i numeri random. si conclude con la rimozione del d-none su input
-// qui utilizzo un eventlistener sul submit del form che mi prende i valori degli input.
-// poi necessito di una funzione per verificare se gli input sono corretti e indico quali sono.
-// gestisco anche gli errori negli input, come valori duplicati, assenti o NaN.
-//
-
 // TODO: handle duplicate numbers in array gen
 // TODO: handle input errors
 function generateRandomNumber(min, max) {
@@ -94,7 +79,7 @@ answersForm.addEventListener("submit", (event) => {
   }
 
   // comunicate to user if he won/lost and the [correctValues]
-  if (randomNumbers > correctValues)
+  if (randomNumbers.length > correctValues.length)
     message.textContent = `Hai sbagliato. Ti sei ricordato solo questi numeri ${correctValues}`;
   else
     message.textContent = `Congratulazioni! I numeri sono corretti! ${correctValues}`;
